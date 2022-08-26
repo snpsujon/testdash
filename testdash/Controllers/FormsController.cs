55db3ef474a8fa998fa3105bf6c0d62585684e43
SNP Sujon <snpsujon93@gmail.com>
2022-08-26 23:58:46 +0600

    forms

---
+++ testdash/Controllers/FormsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testdash.Controllers
+{
+    public class FormsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("F/Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [Route("F/File")]
+        public IActionResult FileUpload()
+        {
+            return View();
+        }
+        [Route("F/Repeater")]
+        public IActionResult Repeater()
+        {
+            return View();
+        }
+
+    }
+}
