733713b86c3f98208b66ccb3f0042585d1eba5e0
SNP Sujon <snpsujon93@gmail.com>
2022-08-27 10:59:57 +0600

    fcgcf

--- testdash/Controllers/FormsController.cs
+++ testdash/Controllers/FormsController.cs
@@ -3,18 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using testdash.StaticClasses;
 
 namespace testdash.Controllers
 {
     public class FormsController : Controller
     {
+        public static string id = RandomGenarator.RandomString(5);
+        public static class GlobalValues
+        {
+            public static string data = RandomGenarator.RandomString(5);
+        }
         public IActionResult Index()
         {
+            
             return View();
         }
-        [Route("F/Create")]
+        [Route("F/Create/{id?}")]
         public IActionResult Create()
         {
+            
             return View();
         }
         [Route("F/File")]
